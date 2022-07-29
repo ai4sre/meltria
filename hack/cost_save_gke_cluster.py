6a38d9ecc68ba793606bdd8bd1ad472ef957332e
yuuki <yuki.tsubo@gmail.com>
2022-07-29 16:54:49 +0900

    hack: add a tool for degrading and recovering a gke cluster

---
+++ hack/cost_save_gke_cluster.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import logging
+import subprocess
+from ctypes import resize
+
+GKE_CMD = 'gcloud -q container'
+
+
+def get_node_pools(cluster_name: str, zone: str) -> list:
+    cmd = f'{GKE_CMD} node-pools list --cluster {cluster_name} -z {zone} --format "json"'
+    logging.info(f"Running {cmd} ...")
+    node_pools = json.loads(subprocess.check_output(cmd, shell=True).decode('utf-8').strip())
+    return node_pools
+
+
+def resize_node_pool(cluster_name: str, zone: str, pool_name: str, size: int):
+    cmd = f'{GKE_CMD} clusters resize {cluster_name} -z {zone} --node-pool={pool_name} --num-nodes={size}'
+    logging.info(f"Running {cmd} ...")
+    subprocess.check_call(cmd, shell=True)
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--up', action='store_true', help='Recover the cluster')
+    parser.add_argument('--down', action='store_true', help='Degrade the cluster')
+    parser.add_argument('--cluster-name', required=True, help='GKE cluster name')
+    parser.add_argument('--zone', help='gcp zone')
+    parser.add_argument('--recover-file',
+                        default='/tmp/meltria_gke_node_pools_recover.json',
+                        help='File to store the recovery cluster')
+    args = parser.parse_args()
+
+    if args.up and args.down:
+        logging.error('Please specify either --up or --down')
+        exit(1)
+
+    if args.up:
+        pool_to_node_count: dict[str, int] = {}
+        logging.info(f'Loading {args.recover_file} ...')
+        with open(args.recover_file, mode='r') as f:
+            saved_pools = json.load(f)
+            for pool in saved_pools:
+                pool_to_node_count[pool['name']] = pool['initialNodeCount']
+        for pool in get_node_pools(args.cluster_name, args.zone):
+            name = pool["name"]
+            logging.info(f'Recovering node pool {name} in {args.cluster_name}')
+            resize_node_pool(args.cluster_name, args.zone, name, pool_to_node_count[name])
+    elif args.down:
+        pools = get_node_pools(args.cluster_name, args.zone)
+        logging.info(f'Saving pool information into {args.recover_file} for recovering ...')
+        with open(args.recover_file, mode='w') as f:
+            json.dump(pools, f, indent=2)
+        for pool in pools:
+            logging.info(f'Degrading node pool {pool["name"]} in {args.cluster_name}')
+            resize_node_pool(args.cluster_name, args.zone, pool['name'], 0)
+    else:
+        logging.error('Please specify either --up or --down')
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
