783ca830643dfcba6db68e10f8cafbc64fa6f750
yuuki <yuki.tsubo@gmail.com>
2022-08-24 15:26:21 +0900

    collectors: support train ticket in the script for retriving metrics

--- collectors/metrics/bin/retrieve_metrics_from_prom.py
+++ collectors/metrics/bin/retrieve_metrics_from_prom.py
@@ -5,7 +5,7 @@ import json
 import logging
 import os
 
-from targets import sockshop
+from targets import sockshop, trainticket
 
 DEFAULT_DURATION = "30m"
 
@@ -24,6 +24,7 @@ def main():
                         nargs='+',
                         help='metrics output JSON file')
     parser.add_argument("--duration", help="", type=str, default=DEFAULT_DURATION)
+    parser.add_argument("--target", help="target application", type=str, default=sockshop.TARGET_APP_NAME)
     parser.add_argument('destdir', help='destionation directory')
     args = parser.parse_args()
 
@@ -31,18 +32,35 @@ def main():
         logging.info(f"--> Loading {metrics_file} ...")
         with open(metrics_file) as r:
             obj = json.load(r)
-            result = sockshop.collect_metrics(
-                prometheus_url=obj['meta']['prometheus_url'],
-                grafana_url=obj['meta']['grafana_url'],
-                start_time=str(obj['meta']['start']),
-                end_time=str(obj['meta']['end']),
-                step=obj['meta']['step'],
-                duration=DEFAULT_DURATION,  # FIXME: calculate duration from start and end
-                chaos_param={
-                    'chaos_injected_component': obj['meta']['chaos_injected_component'],
-                    'injected_chaos_type': obj['meta']['injected_chaos_type'],
-                },
-            )
+            match args.target:
+                case sockshop.TARGET_APP_NAME:
+                    result = sockshop.collect_metrics(
+                        prometheus_url=obj['meta']['prometheus_url'],
+                        grafana_url=obj['meta']['grafana_url'],
+                        start_time=str(obj['meta']['start']),
+                        end_time=str(obj['meta']['end']),
+                        step=obj['meta']['step'],
+                        duration=DEFAULT_DURATION,  # FIXME: calculate duration from start and end
+                        chaos_param={
+                            'chaos_injected_component': obj['meta']['chaos_injected_component'],
+                            'injected_chaos_type': obj['meta']['injected_chaos_type'],
+                        },
+                    )
+                case trainticket.TARGET_APP_NAME:
+                    result = trainticket.collect_metrics(
+                        prometheus_url=obj['meta']['prometheus_url'],
+                        grafana_url=obj['meta']['grafana_url'],
+                        start_time=str(obj['meta']['start']),
+                        end_time=str(obj['meta']['end']),
+                        step=obj['meta']['step'],
+                        duration=DEFAULT_DURATION,  # FIXME: calculate duration from start and end
+                        chaos_param={
+                            'chaos_injected_component': obj['meta']['chaos_injected_component'],
+                            'injected_chaos_type': obj['meta']['injected_chaos_type'],
+                        },
+                    )
+                case _:
+                    raise ValueError("Unknown target: {}".format(args.target))
 
             dest_file = os.path.join(args.destdir, os.path.basename(metrics_file))
             logging.info(f"--> Dumping {dest_file} ...")
